eb0229b74b0a05471448afd71a8c0a0aaa0a237f
Thiru madeshwaran <146909970+Thirumadeshwaran@users.noreply.github.com>
2024-05-15 23:37:08 +0530

    Add files via upload

---
+++ College/src/main/java/com/placement/College/repository/CollegeRepository.java
@@ -0,0 +1,10 @@
+package com.placement.College.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.placement.College.entity.College;
+
+public interface CollegeRepository extends JpaRepository<College, Long>{
+
+	
+}
